6978fdc2eebfe10f59b34158eb011135ef924e57
Sachin Maski <112940168+sachinmaski4932@users.noreply.github.com>
2024-04-30 10:28:25 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,8 @@
+public class add{
+    public static void main(String[] args){
+        int n1= 10;
+        int n2=20;
+        int sum =n1+n2;
+        System.out.println(" the sum of two numbers is"+ sum);
+    }
+}
